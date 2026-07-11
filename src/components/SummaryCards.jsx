5f922793e32a89e9975414e5e7090604ca3e7f85
Deekshitha Poloju <deekshithapoloju18@gmail.com>
2026-07-11 21:20:34 +0530

    Initial commit

--- src/components/SummaryCards.jsx
+++ src/components/SummaryCards.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   FaArrowUp,
   FaArrowDown,
@@ -8,11 +9,35 @@ import {
 import "./SummaryCards.css";
 
 function SummaryCards() {
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  useEffect(() => {
+    const transactions =
+      JSON.parse(localStorage.getItem("transactions")) || [];
+
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    transactions.forEach((transaction) => {
+      if (transaction.type === "Income") {
+        totalIncome += Number(transaction.amount);
+      } else {
+        totalExpense += Number(transaction.amount);
+      }
+    });
+
+    setIncome(totalIncome);
+    setExpense(totalExpense);
+  }, []);
+
+  const savings = income - expense;
+  const balance = income - expense;
+
   return (
     <div className="summary-grid">
 
       {/* Income */}
-
       <div className="card income-card">
         <div className="card-icon">
           <FaArrowUp />
@@ -20,13 +45,12 @@ function SummaryCards() {
 
         <div>
           <h4>Income</h4>
-          <h2>₹75,000</h2>
-          <p>+12% from last month</p>
+          <h2>₹{income.toLocaleString()}</h2>
+          <p>Total Income</p>
         </div>
       </div>
 
       {/* Expense */}
-
       <div className="card expense-card">
         <div className="card-icon">
           <FaArrowDown />
@@ -34,13 +58,12 @@ function SummaryCards() {
 
         <div>
           <h4>Expense</h4>
-          <h2>₹22,550</h2>
-          <p>-5% from last month</p>
+          <h2>₹{expense.toLocaleString()}</h2>
+          <p>Total Expense</p>
         </div>
       </div>
 
       {/* Savings */}
-
       <div className="card savings-card">
         <div className="card-icon">
           <FaPiggyBank />
@@ -48,13 +71,12 @@ function SummaryCards() {
 
         <div>
           <h4>Savings</h4>
-          <h2>₹52,450</h2>
-          <p>Excellent 🎉</p>
+          <h2>₹{savings.toLocaleString()}</h2>
+          <p>Income - Expense</p>
         </div>
       </div>
 
       {/* Balance */}
-
       <div className="card balance-card">
         <div className="card-icon">
           <FaWallet />
@@ -62,8 +84,8 @@ function SummaryCards() {
 
         <div>
           <h4>Total Balance</h4>
-          <h2>₹52,450</h2>
-          <p>Updated Today</p>
+          <h2>₹{balance.toLocaleString()}</h2>
+          <p>Available Balance</p>
         </div>
       </div>
 
